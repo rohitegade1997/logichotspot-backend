df95b022c95cf7d2b783e090642c6806ad7f7143
amitgandole <35072407+amitgandole@users.noreply.github.com>
2020-04-16 00:24:28 +0530

    Demo1 Add New functionality

--- app/Demo1_model.php
+++ app/Demo1_model.php
@@ -12,7 +12,7 @@ class Demo1_model extends Model
     {
         try {
             $demo1_records = DB::table('demo1')
-                ->select('id', 'first_name', 'last_name')
+                ->select('id', 'first_name', 'last_name','indian_phone')
                 ->paginate($demo1_limit);
             return $demo1_records;
         } catch (Exception $obj) {
@@ -24,7 +24,7 @@ class Demo1_model extends Model
     public function search_name($search_keyword)
     {
         try {
-            $names = DB::table('demo1')->select('id', 'first_name', 'last_name')
+            $names = DB::table('demo1')->select('id', 'first_name', 'last_name','indian_phone')
                 ->where('first_name', 'LIKE', '%' . $search_keyword . '%')->get();
             return $names;
         } catch (Exception $obj) {
@@ -35,11 +35,14 @@ class Demo1_model extends Model
     //add new record
     public function add_new_record($request_data)
     {
+        $system_date = date('Y-m-d H:i:s');
         try {
             $name = DB::table('demo1')->insert([
                 "first_name" => $request_data['firstName'],
                 "last_name" => $request_data['lastName'],
-                "phone" => $request_data['phone']
+                "indian_phone" => $request_data['phone'],
+                "created_date" => $system_date,
+                "updated_date" => $system_date
             ]);
             if ($name > 0) {
                 return ["message" => "New record has been added"];
